9ab58091772aa8101b771c6b303d7310d4c102b6
Jon Cronin <jonjoncronin@gmail.com>
2018-05-18 22:29:45 -0700

    Add nativebase react component kit

--- App.js
+++ App.js
@@ -1,23 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Container, Header, Body, Title, Content, Text} from 'native-base';
 
 export default class App extends React.Component {
+  state = { isReady: false };
+
+  async componentWillMount() {
+  await Expo.Font.loadAsync({
+    'Roboto': require('native-base/Fonts/Roboto.ttf'),
+    'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    });
+
+    this.setState({isReady: true});
+  }
+
   render() {
+    if (!this.state.isReady) {
+      return <Expo.AppLoading />;
+    }
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Container>
+        <Header>
+          <Body>
+            <Title>Header</Title>
+          </Body>
+        </Header>
+        <Content>
+          <Text>Open up App.js to start working on your app!</Text>
+        </Content>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
