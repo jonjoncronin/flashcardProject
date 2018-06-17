ebe54227e5b497a345472b1e34faa105d7c88f47
Jon Cronin <jonjoncronin@gmail.com>
2018-06-16 21:29:36 -0700

    Remove development array of initial decks

--- src/utils/dbApi.js
+++ src/utils/dbApi.js
@@ -6,46 +6,46 @@ export const DECKS_STORAGE_KEY = 'flashCard:decks';
 //==============================================================================
 // Setup LocalStorage of flashCard:decks data - prepopulate with dummy data for
 // debugging and development
-const storeDecks = [
-  {
-    id: uuidv1(),
-    shortName: "Store Deck 1",
-    description: "Deck of random cards",
-    cards: [
-      {
-        id: uuidv1(),
-        question: "What is your favorite color?",
-        answer: "Blue"
-      },
-      {
-        id: uuidv1(),
-        question: "What is your quest?",
-        answer: "To seek the Holy Grail"
-      }
-    ],
-    scores: []
-  },
-  {
-    id: uuidv1(),
-    shortName: "Store Deck 2",
-    description: "Deck of more random cards",
-    cards: [
-      {
-        id: uuidv1(),
-        question: "What is your favorite sport?",
-        answer: "Basketball"
-      },
-      {
-        id: uuidv1(),
-        question: "What is Michael Jordans jersey number?",
-        answer: "23"
-      }
-    ],
-    scores: []
-  }
-];
+// const storeDecks = [
+//   {
+//     id: uuidv1(),
+//     shortName: "Store Deck 1",
+//     description: "Deck of random cards",
+//     cards: [
+//       {
+//         id: uuidv1(),
+//         question: "What is your favorite color?",
+//         answer: "Blue"
+//       },
+//       {
+//         id: uuidv1(),
+//         question: "What is your quest?",
+//         answer: "To seek the Holy Grail"
+//       }
+//     ],
+//     scores: []
+//   },
+//   {
+//     id: uuidv1(),
+//     shortName: "Store Deck 2",
+//     description: "Deck of more random cards",
+//     cards: [
+//       {
+//         id: uuidv1(),
+//         question: "What is your favorite sport?",
+//         answer: "Basketball"
+//       },
+//       {
+//         id: uuidv1(),
+//         question: "What is Michael Jordans jersey number?",
+//         answer: "23"
+//       }
+//     ],
+//     scores: []
+//   }
+// ];
 
-AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify(storeDecks));
+AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify([]));
 
 //==============================================================================
 export const getAllDecks = () =>
@@ -56,13 +56,3 @@ export const updateDecks = (newDecks) => {
   console.log("DecksAPI updateDecks: ", newDecks);
   AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify(newDecks));
 }
-
-// export const getDeck = (deckID) => {
-//   let storePromise = AsyncStorage.getItem(DECKS_STORAGE_KEY)
-//     .then((results) => {
-//       const data = JSON.parse(results);
-//       deck = JSON.stringify(data[deckID]);
-//       return deck;
-//     });
-//   return storePromise;
-// }
